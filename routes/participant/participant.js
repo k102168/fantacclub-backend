341d8212e750424e5752212f3df0527b2f9d25e9
20-50 <31932590+20-50@users.noreply.github.com>
2018-03-25 19:14:04 +0500

    update participant.js

--- routes/participant/participant.js
+++ routes/participant/participant.js
@@ -1,3 +1,52 @@
 const router =  require('express').Router();
-const participant=require('../../models/participants');
+const Participant=require('../../models/participants');
 const mongoose=require('mongoose');
+
+//get all the participants
+router.get('/participant',(rq,res,next)=>   {
+    Participant.find({},function(err,results){
+        console.log(results)
+        res.status(200).json(results)
+    })
+})
+
+//get only one participants
+router.get('/participant/:participantid',(req,res,next)=>{
+const id=req.params.particpantid;
+Participant.findById(id, (err, results)=>{
+    res.status(200).json(results)
+} );
+})
+
+
+
+
+
+router.post('/participants',(req,res,next)=>{
+const  {
+    name,
+    phoneNumber,
+    email,
+    wins
+}=req.body;
+const _id = new mongoose.Types.ObjectId();
+const participant=new Participant({
+    name,
+    phoneNumber,
+    email,
+    wins
+})
+
+participant.save().then(results=>{
+    res.status(200).json(result);
+}).catch( err =>{
+    res.status(500).json({
+        error: err
+    })
+})
+
+
+
+}
+
+})
